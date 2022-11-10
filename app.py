de45ac6ac44c413868c55d64fa5955fbb0b2fc47
James <james.john.xml@gmail.com>
2022-11-10 08:04:33 +0100

    First case working

--- app.py
+++ app.py
@@ -5,6 +5,21 @@ from faunadb import query as q
 from faunadb.client import FaunaClient
 
 app = Flask(__name__)
+client = FaunaClient(secret="your-secret-here", domain="db.us.fauna.com", scheme="https")
+# there are other options of region to use for your domain
+
+def generate_identifier(n=6):
+    identifier = ""
+    for i in range(n):
+        identifier += random.choice(string.ascii_letters)
+    return identifier
+
+
+@app.route("/")
+def home():
+    return "Hello World!"
+
+
 
 
 
